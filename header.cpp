1770aaee5a21a836339a0a52513e92366a3dfd4a
kc <151539784+33kc@users.noreply.github.com>
2025-01-03 18:19:16 +0530

    randomised pipe height (after 9 months)

--- header.cpp
+++ header.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <conio.h> // only for Windows
+#include <conio.h>
 #include <windows.h> 
 
 const int WIDTH = 20;
 const int HEIGHT = 10;
 const char BIRD_CHAR = '@';
 const char PIPE_CHAR = '|';
+const int PIPE_GAP = 4;
 
-void drawGame(int birdY, int pipeX) {
+void drawGame(int birdY, int pipeX, int topPipeHeight) 
+{
     system("cls");
     for (int i = 0; i < HEIGHT; ++i) {
-        for (int j = 0; j < WIDTH; ++j) {
-            if ((j == pipeX || j == pipeX + 1) && (i == 0 || i == HEIGHT - 1)) {
-                std::cout << PIPE_CHAR;
-            } else if (j == WIDTH / 4 && i == birdY) {
+        for (int j = 0; j < WIDTH; ++j)
+        {
+            if (j == pipeX) {
+                if (i < topPipeHeight)
+                {
+                    std::cout << PIPE_CHAR;
+                } else if (i >= topPipeHeight + PIPE_GAP) {
+                    std::cout << PIPE_CHAR;
+                } else {
+                    std::cout << " ";
+                }
+            }
+            else if (j == WIDTH / 4 && i == birdY) {
                 std::cout << BIRD_CHAR;
-            } else {
+            }
+            else {
                 std::cout << " ";
             }
         }
         std::cout << std::endl;
     }
 }
+
 int main() {
     int birdY = HEIGHT / 2;
     int pipeX = WIDTH;
+    int topPipeHeight = rand() % (HEIGHT - PIPE_GAP);
     srand(time(NULL));
+
     while (true) {
         if (_kbhit()) {
             char key = _getch();
@@ -35,16 +50,22 @@ int main() {
                 birdY -= 2;
             }
         }
-        birdY++; 
-        pipeX--; 
+        
+        birdY++;
+        pipeX--;
+
+        if (pipeX < 0) {
+            pipeX = WIDTH;
+            topPipeHeight = rand() % (HEIGHT - PIPE_GAP);
+        }
+
+        drawGame(birdY, pipeX, topPipeHeight);
+
         if (birdY <= 0 || birdY >= HEIGHT) {
             std::cout << "Game Over!" << std::endl;
             break;
         }
-        if (pipeX < 0) {
-            pipeX = WIDTH;
-        }
-        drawGame(birdY, pipeX);
+
         Sleep(100);
     }
     return 0;
